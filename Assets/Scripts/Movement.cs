9368a536a88f22ec978bafaba0ce21cc1a833beb
Alexander Georgiev <alexandur04@abv.bg>
2019-03-15 14:48:42 +0200

    added charachter controller

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Animator animator;
+    void Update()
+    
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+
+
+        transform.position = transform.position + movement * Time.deltaTime;
+
+    
+    
+    
+    
+    }
+
+
+
+}
